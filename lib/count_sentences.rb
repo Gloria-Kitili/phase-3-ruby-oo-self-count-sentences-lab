67f9ae01f1f6556cfae1989440ad23aa1d3e788b
root <gloria.kitili@student.moringaschool.com>
2023-05-19 13:32:58 +0300

    self,end_with?

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,26 @@
 require 'pry'
-
-class String
-
+class String 
   def sentence?
-    self.end_with?('.')
+    self.end_with?(".")
   end
 
   def question?
-   self.end_with?('?')
+    self.end_with?("?")
   end
 
   def exclamation?
-   self.end_with?('!')
+    self.end_with?("!")
   end
 
   def count_sentences
-    self.split(/[.?!]/).reject(&:empty?).size
+    self.split(/[.?!]/).reject(&:empty?).count
   end
-end
+end 
+
+#str ="Hello world!"
+#puts str.sentence? #false
+#puts str.question? #false
+#puts str.exclamation  #true
+
+#str = "This is a sentence. This is another sentence? And this is a third sentence!"
+#puts str.count_sentence #3
